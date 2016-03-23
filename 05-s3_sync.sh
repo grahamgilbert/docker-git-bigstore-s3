b4f9afece011a188a108a246114ae62c15894018
Graham Gilbert <graham@grahamgilbert.com>
2016-03-23 16:34:51 +0000

    Clone locally then sync

---
+++ 05-s3_sync.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Get s3 creds from env var and write to password file
+mdir -p ~/.aws
+echo '[default]\naws_access_key_id=${S3_IDENTITY}\naws_secret_access_key=${S3_CREDENTIAL}' >> ~/.aws/credentials
+# Mount s3 bucket from environment variable
+# mkdir -p ${TARGET_DIR}
+# s3fs -o nonempty $S3_BUCKET ${TARGET_DIR}
+aws s3 sync ${TARGET_DIR} s3://$S3_BUCKET
