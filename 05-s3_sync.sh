6ed5a83ebabc95d37de9d2c74fa7fdb60bb78521
Graham Gilbert <graham@grahamgilbert.com>
2016-03-24 11:49:58 +0000

    Right format for credential file

--- 05-s3_sync.sh
+++ 05-s3_sync.sh
@@ -2,7 +2,9 @@
 
 # Get s3 creds from env var and write to password file
 mkdir -p ~/.aws
-echo '[default]\naws_access_key_id=${S3_IDENTITY}\naws_secret_access_key=${S3_CREDENTIAL}' >> ~/.aws/credentials
+echo '[default]' >> ~/.aws/credentials
+echo "aws_access_key_id=${S3_IDENTITY}" >> ~/.aws/credentials
+echo "aws_secret_access_key=${S3_CREDENTIAL}" >> ~/.aws/credentials
 # Mount s3 bucket from environment variable
 # mkdir -p ${TARGET_DIR}
 # s3fs -o nonempty $S3_BUCKET ${TARGET_DIR}
